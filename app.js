13c85465a1be9f8a02780e4d1ef3698d7a406835
Kazim Shabbir <59815134+Kazim786@users.noreply.github.com>
2020-08-28 19:53:22 -0500

    Added app.js

---
+++ app.js
@@ -0,0 +1,16 @@
+//Created server 
+
+const http = require('http')
+
+//RequestListener takes in request and response arguments
+const requestListener = (req, res) => {
+    console.log(req)
+}
+
+
+//The method that goes into createServer is called a requestListener. I explicitly named the method i put into this a requestListener, You can name it anything
+//To use this you have to save it to a variable. I called mine server explicitly. Again you can name it anything
+const server = http.createServer(requestListener)
+
+//server comes with many methods one of them is listen.
+server.listen(3000);
