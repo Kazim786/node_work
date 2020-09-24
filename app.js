29389820e18b8e0afff6d885c747462a7a4c0665
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-09-24 00:06:08 -0500

    made my own 404 and made its html serve

--- app.js
+++ app.js
@@ -35,6 +35,8 @@
 //***************** */
 //Gonna work with Routes and middleware now 
 
+const path = require('path')
+
 const express = require('express');
 
 const bodyParser = require('body-parser') //now requests body object will get parsed as soon as body parser is put into app.use middleware. It parses JSON requests
@@ -52,8 +54,15 @@ app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 //404 page
+// app.use((req, res, next) => {
+//     res.status(404).send('<h1>Page not Found</h1>')
+// })
+
+//serving 404 html page
+
 app.use((req, res, next) => {
-    res.status(404).send('<h1>Page not Found</h1>')
+    //
+    res.status(404).sendFile(path.join(__dirname, './', 'views', 'notfound.html'))
 })
 
 
